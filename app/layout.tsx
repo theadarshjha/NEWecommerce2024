a32a6683f0451e28b386f47bbfecca80002dfe6d
thejhaadarsh <thejhaadarsh@gmail.com>
2024-08-29 16:22:10 +0530

    update on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { CartProvider } from '@/components/contexts/CartContext';
 
 
 export const metadata: Metadata = {
@@ -15,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body >
+      <CartProvider>
+        {children}
+        </CartProvider>
+        </body>
     </html>
   );
 }
